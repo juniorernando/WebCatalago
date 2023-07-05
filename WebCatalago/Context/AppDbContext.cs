f6e4eb84f1c030e2ceb9cf4eac82bb18d26429f0
Junior Fernando <junior_618@hotmail.com>
2023-07-04 21:08:36 -0300

    Adicionar arquivos de projeto.

---
+++ WebCatalago/Context/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebCatalogo.Models;
+
+namespace WebCatalogo.Context
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        { }
+
+        public DbSet<Categoria>? Categorias { get; set; }
+        public DbSet<Produto>? Produtos { get; set; }
+
+    }
+}
